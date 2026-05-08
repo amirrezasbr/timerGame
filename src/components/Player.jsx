60e6cef5cd3b618fecd2753bccc8c9c0141b1a81
unknown <amirreza13.7676@gmail.com>
2026-05-08 17:03:25 +0330

    implement the get the input value with the help of ref hook

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,25 +1,49 @@
-import { useState } from "react";
+// get the input value with the help of useState and ref hook only
+
+import { useState, useRef } from "react";
 
 export default function Player() {
+  const playerName = useRef();
   const [enteredPlayerName, setenteredPlayerName] = useState("");
-  const [submitted, setSubmitted] = useState(false);
-
-  function handleChange(event) {
-    setSubmitted(false);
-    setenteredPlayerName(event.target.value);
-  }
 
   function handleClick() {
-    setSubmitted(true);
+    setenteredPlayerName(playerName.current.value);
   }
 
   return (
     <section id="player">
-      <h2>Welcome {submitted ? enteredPlayerName : "unkown entity"}</h2>
+      <h2>Welcome {enteredPlayerName ?? "unkown entity"}</h2>
       <p>
-        <input type="text" onChange={handleChange} value={enteredPlayerName} />
+        <input ref={playerName} type="text" />
         <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
 }
+
+// get the input value with the help of useState hook only this is here for only comparison
+// import { useState } from "react";
+
+// export default function Player() {
+//   const [enteredPlayerName, setenteredPlayerName] = useState("");
+//   const [submitted, setSubmitted] = useState(false);
+
+//   function handleChange(event) {
+//     setSubmitted(false);
+//     setenteredPlayerName(event.target.value);
+//   }
+
+//   function handleClick() {
+//     setSubmitted(true);
+//   }
+
+//   return (
+//     <section id="player">
+//       <h2>Welcome {submitted ? enteredPlayerName : "unkown entity"}</h2>
+//       <p>
+//         <input type="text" onChange={handleChange} value={enteredPlayerName} />
+//         <button onClick={handleClick}>Set Name</button>
+//       </p>
+//     </section>
+//   );
+// }
