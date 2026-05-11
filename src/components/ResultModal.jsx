b653280e06932116d300724cf5b3bd8e3691675a
unknown <amirreza13.7676@gmail.com>
2026-05-11 23:45:21 +0330

    wining dialog is now working

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,8 +1,18 @@
 // react 19 or above method
 import { useImperativeHandle, useRef } from "react";
+import { createPortal } from "react-dom";
 
-export default function ResultModal({ ref, result, targetTime }) {
+export default function ResultModal({
+  ref,
+  targetTime,
+  remainingTime,
+  onReset,
+}) {
   const dialog = useRef();
+
+  const userLost = remainingTime <= 0;
+  const formattedRemaingTime = (remainingTime / 1000).toFixed(2);
+  const score = Math.round((1 - remainingTime / (targetTime * 1000)) * 100);
   useImperativeHandle(ref, () => {
     return {
       open() {
@@ -11,19 +21,22 @@ export default function ResultModal({ ref, result, targetTime }) {
     };
   });
 
-  return (
+  return createPortal(
     <dialog ref={dialog} className="result-modal">
-      <h2>You {result}</h2>
+      {userLost && <h2>You Lost</h2>}
+      {!userLost && <h2>You Score:{score}.</h2>}
       <p>
         The target time was <strong>{targetTime} seconds.</strong>
       </p>
       <p>
-        You stopped the timer with <strong>X seconds left.</strong>
+        You stopped the timer with{" "}
+        <strong>{formattedRemaingTime} seconds left.</strong>
       </p>
-      <form method="dialog">
+      <form method="dialog" onSubmit={onReset}>
         <button>Close</button>
       </form>
-    </dialog>
+    </dialog>,
+    document.getElementById("modal"),
   );
 }
 
